7e3d372fa224140dc9f2324a47902466be32929e
Henry Le <henryle913@outlook.com>
2025-04-25 10:00:12 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>Adding more to this App</div>
+    <div role='button'>Some button</div>
+    <div role='header'>Some header content</div>
   )
 }
 
